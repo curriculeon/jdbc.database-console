5d7d0ff37922de92f443df4511986f96b9a105c0
Emmanuel Orubele <blontf@gmail.com>
2020-08-15 23:38:10 -0400

    JDBC connection and assignment

---
+++ src/main/java/com/github/perschola/AnsiColor.java
@@ -0,0 +1,28 @@
+package com.github.perschola;
+
+public enum AnsiColor {
+     /**
+      * used to create color for output of `IOConsole`
+      *
+      */
+        AUTO("\u001B[0m"),
+        BLACK("\u001B[30m"),
+        RED("\u001B[31m"),
+        GREEN("\u001B[32m"),
+        YELLOW("\u001B[33m"),
+        BLUE("\u001B[34m"),
+        PURPLE("\u001B[35m"),
+        CYAN("\u001B[36m"),
+        WHITE("\u001B[37m");
+
+        private final String color;
+
+        AnsiColor(String ansiColor) {
+            this.color = ansiColor;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+    }
--- src/main/java/com/github/perschola/MyObject.java
+++ src/main/java/com/github/perschola/MyObject.java
@@ -1,47 +1,48 @@
 package com.github.perschola;
 
 import java.sql.*;
-import com.mysql.cj.jdbc.Driver;
 import java.util.StringJoiner;
 
 public class MyObject implements Runnable {
+    IOConsole console = new IOConsole();
+    Connection mysqlDbConnection = getConnection("mariadb");;
     public void run() {
         registerJDBCDriver();
-        Connection mysqlDbConnection = getConnection("mysql");
 
-        executeStatement(mysqlDbConnection, "DROP DATABASE IF EXISTS databaseName;");
-        executeStatement(mysqlDbConnection, "CREATE DATABASE IF NOT EXISTS databaseName;");
-        executeStatement(mysqlDbConnection, "USE databaseName;");
-        executeStatement(mysqlDbConnection, new StringBuilder()
+
+        executeStatement("DROP DATABASE IF EXISTS databaseName;");
+        executeStatement("CREATE DATABASE IF NOT EXISTS databaseName;");
+        executeStatement("USE databaseName;");
+        executeStatement(new StringBuilder()
                 .append("CREATE TABLE IF NOT EXISTS databaseName.pokemonTable(")
                 .append("id int auto_increment primary key,")
                 .append("name text not null,")
                 .append("primary_type int not null,")
                 .append("secondary_type int null);").toString());
 
-        executeStatement(mysqlDbConnection, new StringBuilder()
+        executeStatement(new StringBuilder()
                 .append("INSERT INTO databaseName.pokemonTable ")
                 .append("(id, name, primary_type, secondary_type)")
                 .append(" VALUES (12, 'Ivysaur', 3, 7);").toString());
 
         String query = "SELECT * FROM databaseName.pokemonTable;";
-        ResultSet resultSet = executeQuery(mysqlDbConnection, query);
+        ResultSet resultSet = executeQuery(query);
         printResults(resultSet);
     }
 
     void registerJDBCDriver() {
         // Attempt to register JDBC Driver
         try {
-            DriverManager.registerDriver(Driver.class.newInstance());
-        } catch (InstantiationException | IllegalAccessException | SQLException e1) {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
             throw new Error(e1);
         }
     }
 
     public Connection getConnection(String dbVendor) {
         String username = "root";
-        String password = "";
-        String url = "jdbc:" + dbVendor + "://127.0.0.1/";
+        String password = "muchEO21";
+        String url = "jdbc:" + dbVendor + "://localhost:3300/";
         try {
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
@@ -77,22 +78,31 @@ public class MyObject implements Runnable {
         }
     }
 
-    void executeStatement(Connection connection, String sqlStatement) {
+    void executeStatement(String sqlStatement) {
         try {
-            Statement statement = getScrollableStatement(connection);
+            Statement statement = getScrollableStatement(mysqlDbConnection);
             statement.execute(sqlStatement);
         } catch (SQLException e) {
             throw new Error(e);
         }
     }
 
-    ResultSet executeQuery(Connection connection, String sqlQuery) {
+    ResultSet executeQuery(String sqlQuery) {
         try {
-            Statement statement = getScrollableStatement(connection);
+            Statement statement = getScrollableStatement(mysqlDbConnection);
             return statement.executeQuery(sqlQuery);
         } catch (SQLException e) {
             throw new Error(e);
         }
     }
+
+    public void  addToTable(Connection connection){
+    }
+
+    public void removeFromTable(){}
+
+    public void updateTable(){}
+
+    public void getField(){}
 
 }
